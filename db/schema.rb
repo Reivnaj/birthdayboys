c412337e1b75561855ec03024398810f03988c1c
Reivnaj <pelletierjanvier@gmail.com>
2021-10-21 18:48:23 +0200

    Switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_05_140828) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "first_name", null: false
